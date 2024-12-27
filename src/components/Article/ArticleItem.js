19c7febf59e7b6d1e9d245065038a40d410075cb
Mehdi <mehdi.organji.wd@gmail.com>
2024-12-27 18:51:18 +0330

    add and delete article

--- src/components/Article/ArticleItem.js
+++ src/components/Article/ArticleItem.js
@@ -3,26 +3,26 @@ import "./ArticleItem.css";
 import Card from "react-bootstrap/Card";
 import { IoMdTime } from "react-icons/io";
 import { TiArrowLeftThick } from "react-icons/ti";
-import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 
-function ArticleItem({ image, title, desc, writter, readingtime }) {
+function ArticleItem({ image, title, desc, writter, readingtime,id }) {
   return (
     <Card>
-      <Card.Img
-        variant="top"
-        src={image}
-      />
+      <Card.Img variant="top" src={image} />
       <Card.Body>
         <Card.Title className="py-2">{title} </Card.Title>
-        <Card.Text>{desc}
-        </Card.Text>
-        <div className="read-more">
-          <span> اطلاعات دوره</span>
-          <TiArrowLeftThick size='25px' />
-        </div>
+        <Card.Text>{desc}</Card.Text>
+        <Link to={`/article/${id}`}>
+          <div className="read-more">
+            <span> ادامه مقاله</span>
+            <TiArrowLeftThick size="25px" />
+          </div>
+        </Link>
       </Card.Body>
-      <CardFooter className="d-flex justify-content-between align-items-center py-3" style={{color: '#5d5c5c'}}>
+      <CardFooter
+        className="d-flex justify-content-between align-items-center py-3"
+        style={{ color: "#5d5c5c" }}
+      >
         <span>نویسنده : {writter}</span>
         <span>
           {" "}
